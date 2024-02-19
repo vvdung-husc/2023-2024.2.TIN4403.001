ecc272271f6ee548e771e2a68e3d4129327f606f
dinhquangphu <138598008+quangphudinh@users.noreply.github.com>
2024-02-19 09:09:22 +0700

    19/2_P

--- dinhquangphu_21t1020589/bai1/app/src/main/java/com/example/bai1/MainActivity.java
+++ dinhquangphu_21t1020589/bai1/app/src/main/java/com/example/bai1/MainActivity.java
@@ -20,7 +20,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+//
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
