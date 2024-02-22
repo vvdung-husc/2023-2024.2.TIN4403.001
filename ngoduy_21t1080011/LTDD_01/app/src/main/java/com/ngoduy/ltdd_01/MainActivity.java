c37fc1882eae94cc127eb735ea2b5c6f5e4b8898
Ngô Duy <ngoduy07142003@gmail.com>
2024-02-22 09:56:21 +0700

    first-commit-3

--- ngoduy_21t1080011/LTDD_01/app/src/main/java/com/ngoduy/ltdd_01/MainActivity.java
+++ ngoduy_21t1080011/LTDD_01/app/src/main/java/com/ngoduy/ltdd_01/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText loiginUsername,loginPassword;
@@ -21,7 +22,14 @@ public class MainActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                String usename = "ngoduy";
+                String password = "12345";
+                if(loiginUsername.getText().toString().equals(usename) && loginPassword.getText().toString().equals(password)) {
+                    String name = loiginUsername.getText().toString();
+                    Toast.makeText(getApplicationContext(), "Đăng nhập thành công:"+name , Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Đăng nhập thất bại", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
