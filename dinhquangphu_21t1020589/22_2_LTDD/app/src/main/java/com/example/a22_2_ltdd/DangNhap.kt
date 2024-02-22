54a09e4ba255841669598805adca575a88688273
dinhquangphu <138598008+quangphudinh@users.noreply.github.com>
2024-02-22 10:16:11 +0700

    22/2

---
+++ dinhquangphu_21t1020589/22_2_LTDD/app/src/main/java/com/example/a22_2_ltdd/DangNhap.kt
@@ -0,0 +1,11 @@
+package com.example.a22_2_ltdd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DangNhap : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dang_nhap)
+    }
+}
