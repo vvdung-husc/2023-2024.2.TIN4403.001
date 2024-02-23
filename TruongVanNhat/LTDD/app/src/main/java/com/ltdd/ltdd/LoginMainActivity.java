2b8063e8f0a8f100251071fd1282f7adc6e8dbd8
lenovo <vannhat1975v@gmail.com>
2024-02-23 09:25:12 +0700

    ngay23

--- TruongVanNhat/LTDD/app/src/main/java/com/ltdd/ltdd/LoginMainActivity.java
+++ TruongVanNhat/LTDD/app/src/main/java/com/ltdd/ltdd/LoginMainActivity.java
@@ -3,17 +3,12 @@ package com.ltdd.ltdd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    public void kick(View v){
-        Log.d("ok1", "kick: ");
+        setContentView(R.layout.activity_login_main);
     }
 }
