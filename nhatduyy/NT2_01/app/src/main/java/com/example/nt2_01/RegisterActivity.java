650dd1b49a4d94dd261aaf3b777472fb2edb313d
Nhatduy249 <20t1020022@husc.edu.vn>
2024-02-23 09:18:55 +0700

    chjfy

--- nhatduyy/NT2_01/app/src/main/java/com/example/nt2_01/RegisterActivity.java
+++ nhatduyy/NT2_01/app/src/main/java/com/example/nt2_01/RegisterActivity.java
@@ -1,14 +1,14 @@
- package com.example.nt2_01;
+package com.example.nt2_01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 }
