37d34db58eca15a6913e680fc90052dfe9d78706
Đoàn Anh Tú <21t1020094@husc.edu.vn>
2024-02-26 09:02:13 +0700

    as asd

---
+++ DoanAnhTu_21t1020094/DoanAnhTu_21T1020094/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DoanAnhTu_21T1020094"
+include(":app")
+ 
