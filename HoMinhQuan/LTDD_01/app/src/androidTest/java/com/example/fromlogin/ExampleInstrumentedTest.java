46895d081575fe086a4ff56e44e58defc5640715
hmquandec05 <quan.m2003@gmail.com>
2024-02-26 08:55:53 +0700

    26/2

--- HoMinhQuan/LTDD_01/app/src/androidTest/java/com/example/fromlogin/ExampleInstrumentedTest.java
+++ HoMinhQuan/LTDD_01/app/src/androidTest/java/com/example/fromlogin/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.ltdd.testing;
+package com.example.fromlogin;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.ltdd.testing", appContext.getPackageName());
+        assertEquals("com.example.fromlogin", appContext.getPackageName());
     }
 }
--- HoMinhQuan/LTDD_01/app/src/main/java/com/example/fromlogin/FormLogin.java
+++ HoMinhQuan/LTDD_01/app/src/main/java/com/example/fromlogin/FormLogin.java
@@ -1,14 +1,14 @@
-package com.ltdd.testing;
+package com.example.fromlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class RegisterActivity extends AppCompatActivity {
+public class FormLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_login);
     }
 }
