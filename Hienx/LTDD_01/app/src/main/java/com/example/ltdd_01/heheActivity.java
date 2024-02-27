a1922eb006eb717ef2781cffc7597222b7352140
hien1021 <144500625+hien1021@users.noreply.github.com>
2024-02-27 14:30:35 +0700

    hí hí

---
+++ Hienx/LTDD_01/app/src/main/java/com/example/ltdd_01/heheActivity.java
@@ -0,0 +1,14 @@
+package com.example.ltdd_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class heheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hehe);
+    }
+}
