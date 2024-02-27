9602cf4249df1774ab7f03f25bf10b440b752f59
lenovo <vannhat1975v@gmail.com>
2024-02-27 09:18:43 +0700

    ngay27

---
+++ TruongVanNhat/LTDD/app/src/main/java/com/ltdd/ltdd/activity_register.java
@@ -0,0 +1,14 @@
+package com.ltdd.ltdd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
