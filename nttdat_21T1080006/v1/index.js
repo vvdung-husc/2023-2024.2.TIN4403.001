db1332512a324a09a64e2d6231ccf9bbca0c5173
21T1080006 <21T1080006@husc.edu.vn>
2024-02-29 09:20:51 +0700

    api29/02/2024

--- nttdat_21T1080006/v1/index.js
+++ nttdat_21T1080006/v1/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 console.log("HELLO NODEJS")
 //sử dụng để kiểm tra API có đang hoạt động
 app.get("/", function (req, res) {
-  res.status(200).send("Welcome to RESTFUL API - NODEJS - TIN4403");
+  res.status(200).send("Welcome to RESTFUL API - NODEJS - TIN4403 - Nguyen Tam Tuan Dat");
 });
 
 //dịch vụ WebService chạy tại cổng số n
