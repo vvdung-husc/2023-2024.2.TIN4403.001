f182b867b9b4f3e2b90f0ad8bf707c823b62b126
DuongMinhChauK45 <21T1020119@husc.edu.vn>
2024-03-02 00:35:26 +0700

    mchau

--- DuongMinhChauK45/LapTrinhDiDong/app/src/main/java/com/ltdd/codeappsamezalo/MainActivity.java
+++ DuongMinhChauK45/LapTrinhDiDong/app/src/main/java/com/ltdd/codeappsamezalo/MainActivity.java
@@ -1,14 +1,46 @@
 package com.ltdd.codeappsamezalo;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private EditText usernameInput;
+    private EditText passwordInput;
+    private Button loginBtn;
+    private Button registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usernameInput = findViewById(R.id.username_input);
+        passwordInput = findViewById(R.id.password_input);
+        loginBtn = findViewById(R.id.login_btn);
+        registerBtn = findViewById(R.id.register_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = usernameInput.getText().toString();
+                String password = passwordInput.getText().toString();
+                Log.i("Test Credentials", "Username: " + username + " and Password: " + password);
+            }
+        });
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = usernameInput.getText().toString();
+                String password = passwordInput.getText().toString();
+                Log.i("Test Credentials", "Username: " + username + " and Password: " + password);
+            }
+        });
     }
 }
