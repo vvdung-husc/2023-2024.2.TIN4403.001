a081e44def86e798e8c056439052427de016821f
Hchung25 <ngocchung252803@gmail.com>
2024-03-05 09:44:28 +0700

    hnchung

---
+++ hoangngocchung/LTUD_01/app/src/main/java/com/hnc/ltud_01/MainActivity.java
@@ -0,0 +1,26 @@
+package com.hnc.ltud_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.text.HtmlCompat;
+import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.io.IOException;
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
