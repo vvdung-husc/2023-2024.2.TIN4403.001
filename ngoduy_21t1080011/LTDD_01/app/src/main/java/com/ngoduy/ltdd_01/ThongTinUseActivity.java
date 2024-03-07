179df793da2766233499edad1c5087b241811dfb
Ngô Duy <ngoduy07142003@gmail.com>
2024-03-07 16:01:24 +0700

    commit_3/7/2024

---
+++ ngoduy_21t1080011/LTDD_01/app/src/main/java/com/ngoduy/ltdd_01/ThongTinUseActivity.java
@@ -0,0 +1,14 @@
+package com.ngoduy.ltdd_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThongTinUseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thong_tin_use);
+    }
+}
