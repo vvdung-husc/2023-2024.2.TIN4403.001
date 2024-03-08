866a0689788b378a1f74dc5cbf9ad6a8d5ddceda
thongvlog27 <20t1020574@husc.edu.vn>
2024-03-08 09:42:12 +0700

    thong

--- thongtv/LTDD_1/app/src/main/java/com/example/ltdd_1/MainActivity.java
+++ thongtv/LTDD_1/app/src/main/java/com/example/ltdd_1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ltdd_1;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.text.HtmlCompat;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
@@ -14,18 +15,24 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.ltdd_1.R;
+import com.example.ltdd_1.UserActivity;
+
 import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-    static String   _usernameLogined;
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    public static String _usernameLogined;
+    static String _URL;
     EditText m_edtUser,m_edtPass; //Biến điều khiển EditText
     Button m_btnLogin; //Biến điều khiển Đăng nhập
     TextView m_lblRegister;//Biến điều khiển Đăng ký mới
@@ -33,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _URL = "https://dev.husc.edu.vn/tin4403/api";
+        _URL = "http://192.168.3.105:4080";
 
         //Khởi tạo các biến điều khiển tương ứng trong layout
         m_edtUser = (EditText)findViewById(R.id.edtUsername);
@@ -56,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
             String pass = m_edtPass.getText().toString();
             Log.d("K45","CLICK BUTTON LOGIN ACCOUNT " + user + "/" + pass);
             if (user.length() < 3 || pass.length() < 6){
-                ShowToast("Tài khoản hoặc mật khẩu không hợp lệ!");
+                ShowToast(getApplicationContext(),"Tài khoản hoặc mật khẩu không hợp lệ!");
                 return;
             }
             try {
@@ -75,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {//Hàm sử lý sự kiện click button register
             //Toast.makeText(getApplicationContext(),"CButtonRegister::onClick...",Toast.LENGTH_SHORT).show();
-            Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+            Intent i = new Intent(getApplicationContext(), com.example.ltdd_1.RegisterActivity.class);
             startActivity(i);
         }
     }//public class CButtonRegister implements View.OnClickListener {
@@ -86,10 +96,10 @@ public class MainActivity extends AppCompatActivity {
         String json = "{\"username\":\"" + user + "\",\"password\":\"" + pass +"\"}";
         Log.d("K45",json);
 
-        boolean bOk = (user.equals("thongtv") && pass.equals("123456"));
+        boolean bOk = (user.equals("vvdung") && pass.equals("123456"));
         if (bOk){
-            _usernameLogined = "Trần Văn Thống";
-            Intent intent = new Intent(getApplicationContext(),UserActivity.class);
+            _usernameLogined = "Võ Việt Dũng";
+            Intent intent = new Intent(getApplicationContext(), UserActivity.class);
             startActivity(intent);
         }
         else{
@@ -98,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
                 public void run() {
                     //Toast.makeText(getApplicationContext(),"Tài khoản hoặc mật khẩu không chính xác.",Toast.LENGTH_SHORT).show();
                     String str = "Tài khoản hoặc mật khẩu không chính xác [" + user + "/" + pass + "]";
-                    ShowToast(str);
+                    ShowToast(getApplicationContext(),str);
                 }
             });
         }
@@ -113,8 +123,7 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         Request request = new Request.Builder()
-                //.url("https://dev.husc.edu.vn/tin4403/api/login")
-                .url("http://192.168.3.116:4080/login")
+                .url(_URL + "/login")
                 .post(body)
                 .build();
         OkHttpClient client = new OkHttpClient();
@@ -155,9 +164,9 @@ public class MainActivity extends AppCompatActivity {
         });//client.newCall(request).enqueue(new Callback() {
     } //void okhttpApiLogin(String user, String pass) throws IOException{
 
-    void ShowToast(String msg){
+    static public void ShowToast(Context ctx, String msg){
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
-            Toast toast = Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_SHORT);
+            Toast toast = Toast.makeText(ctx,msg,Toast.LENGTH_SHORT);
             View view = toast.getView();
             view.setBackgroundColor(Color.GREEN);
             TextView toastMessage = (TextView) toast.getView().findViewById(android.R.id.message);
@@ -165,7 +174,7 @@ public class MainActivity extends AppCompatActivity {
             toast.show();
         }
         else {
-            Toast.makeText(getApplicationContext(),
+            Toast.makeText(ctx,
                     HtmlCompat.fromHtml("<font color='red'>" + msg +"</font>" , HtmlCompat.FROM_HTML_MODE_LEGACY),
                     Toast.LENGTH_LONG).show();
         }
