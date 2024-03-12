e38fa2c715d1323bf13f1a49f28b83b75b2bbf83
DuongMinhChauK45 <21T1020119@husc.edu.vn>
2024-03-12 10:19:36 +0700

    mchau

--- DuongMinhChauK45/LapTrinhDiDong/app/src/main/java/com/ltdd/codeappsamezalo/User.java
+++ DuongMinhChauK45/LapTrinhDiDong/app/src/main/java/com/ltdd/codeappsamezalo/User.java
@@ -2,7 +2,7 @@ package com.ltdd.codeappsamezalo;
 
 public class User {
     public static String _username;
-    public static String _fullname = "<rỗng>";
+    public static String _fullname = MainActivity._usernameLogined;
     public static String _email = "<chưa có>";
 
 }
