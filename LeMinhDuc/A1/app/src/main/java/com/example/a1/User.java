44380f17915a079c756bf9bbca13b75e3e01155a
HP <ducle140503@gmail.com>
2024-03-14 02:19:18 +0700

    ...

---
+++ LeMinhDuc/A1/app/src/main/java/com/example/a1/User.java
@@ -0,0 +1,8 @@
+package com.example.a1;
+
+public class User {
+    public static String _username;
+    public static String _fullname = "<rỗng>";
+    public static String _email = "<chưa có>";
+
+}
