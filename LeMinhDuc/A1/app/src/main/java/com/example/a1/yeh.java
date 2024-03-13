333e9fd0961ff1869a77ddfc63f69df1debe1df1
HP <ducle140503@gmail.com>
2024-03-14 05:43:04 +0700

    ...

--- LeMinhDuc/A1/app/src/main/java/com/example/a1/yeh.java
+++ LeMinhDuc/A1/app/src/main/java/com/example/a1/yeh.java
@@ -2,11 +2,15 @@ package com.example.a1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class yeh extends AppCompatActivity {
     TextView m_lblWelcome,m_lblUser,m_lblName,m_lblEmail;
+    Button m_btnLogout,m_btnChange;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +22,16 @@ public class yeh extends AppCompatActivity {
         m_lblWelcome.setText("Chào "+ User._fullname);
         m_lblName.setText("Username: "+User._username);
         m_lblEmail.setText("Email: "+User._email);
+
+        m_btnLogout = (Button) findViewById(R.id.btnLogout);
+        m_btnLogout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Finish the registration screen and return to the Login activity
+                Intent intent = new Intent(getApplicationContext(), com.example.a1.MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
