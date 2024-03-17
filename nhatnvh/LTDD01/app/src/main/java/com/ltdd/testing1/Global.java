c82f478dce1c8c51cc9420ff1cfec4cfd4fa4597
NhatEric <nguyenvanhoangnhat0406@gmail.com>
2024-03-17 20:49:05 +0700

    nhatnvh

--- nhatnvh/LTDD01/app/src/main/java/com/ltdd/testing1/Global.java
+++ nhatnvh/LTDD01/app/src/main/java/com/ltdd/testing1/Global.java
@@ -17,7 +17,9 @@ import org.json.JSONObject;
 public class Global {
     public static Handler _Handler;
     public static String _token;
-    public static String _URL ="http://192.168.3.136:5080";//"https://dev.husc.edu.vn/tin4403/api";
+    public static String _URL = "https://dev.husc.edu.vn/tin4403/api";
+    // "http://192.168.1.11:5080";
+
     public static API _HTTP = new API(_URL);
 
     public static void ShowToast(Context ctx, String msg){
