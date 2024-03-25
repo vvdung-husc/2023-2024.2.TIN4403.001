c458a8c13704aa8639b88e3c5bd7ab63173a0060
Nguyễn Phúc Tài <taiphuc192@gmail.com>
2024-03-25 11:56:41 +0700

    update

--- NguyenPhucTai/NPT_01/app/src/androidTest/java/com/example/npt_01/ExampleInstrumentedTest.java
+++ NguyenPhucTai/NPT_01/app/src/androidTest/java/com/example/npt_01/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.dat_01;
+package com.example.npt_01;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.dat_01", appContext.getPackageName());
+        assertEquals("com.example.npt_01", appContext.getPackageName());
     }
 }
