e0322e9b1b7aa00cb3e213799b6cb9e04f9ede3f
Bahuy24503 <99529408+Bahuy24503@users.noreply.github.com>
2024-03-26 11:17:32 +0700

    26/3

--- HoangVoBaHuy/LTDD_2/app/src/main/java/com/example/myapplicationhuy/Changepass.java
+++ HoangVoBaHuy/LTDD_2/app/src/main/java/com/example/myapplicationhuy/Changepass.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class confirm_Login extends AppCompatActivity {
+public class Changepass extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_confirm_login);
+        setContentView(R.layout.activity_changepass);
     }
 }
---
+++ HoangVoBaHuy/LTDD_2/app/src/main/java/com/example/myapplicationhuy/Global.java
@@ -0,0 +1,2 @@
+package com.example.myapplicationhuy;public class Global {
+}
