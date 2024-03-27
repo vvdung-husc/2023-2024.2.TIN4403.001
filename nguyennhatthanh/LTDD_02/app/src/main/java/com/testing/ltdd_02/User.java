4d8bbd133bc896897574a35a567f063c054c328e
Boye2022 <20T1020555@husc.edu.vn>
2024-03-27 15:42:24 +0700

    nhatthanh

---
+++ nguyennhatthanh/LTDD_02/app/src/main/java/com/testing/ltdd_02/User.java
@@ -0,0 +1,8 @@
+package com.testing.ltdd_02;
+
+public class User {
+    public static String _username;
+    public static String _fullname = "<rỗng>";
+    public static String _email = "<chưa có>";
+
+}
